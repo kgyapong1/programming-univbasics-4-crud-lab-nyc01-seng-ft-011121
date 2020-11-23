706d2317e61ece4bbacd78a59fe9f0d2ee5b6eb6
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-11-23 20:28:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,12 +1,14 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
-  
+famous_cats = ["lil' bub", "grumpy cat", "Maru" , "Kwaku"]
 end
 
 def add_element_to_end_of_array(array, element)
+  
   
 end
 
